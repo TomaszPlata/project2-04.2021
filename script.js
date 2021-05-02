32cdaaa612dbaa521e16e2fb5ffe8605dca4c48d
Rex <rex@wp.pl>
2021-05-02 13:50:12 +0200

    a lot of new changes

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 let string = ['Volvo', 'Audi', 'Mercedes', 'BMW'];
+let months = ['March', 'April', 'May', 'June'];
 let date1 = new Date();
 
 function showCars() {
     document.getElementById('cars').innerHTML = string;
     document.getElementById('date').innerHTML = date1;
+}
+
+function howLive() {
+    document.getElementById('life').style.cursor = pointer;
 }
